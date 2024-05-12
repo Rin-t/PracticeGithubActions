72ec8e75edb1990bfb07ecd99833e0a5a9d5d43d
Rin-t <rin.t.4921@gmail.com>
2024-05-12 10:53:03 +0900

    fastfileの追加

---
+++ PracticeCICD/PracticeCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeCICDApp.swift
+//  PracticeCICD
+//
+//  Created by Rin on 2024/05/12.
+//
+
+import SwiftUI
+
+@main
+struct PracticeCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
